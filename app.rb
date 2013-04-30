0b4b3b4872338d41f85d0ed8738794659bdff789
chang.christopher@gmail.com <chang.christopher@gmail.com>
2013-04-30 15:36:21 -0700

    fixed db config

--- app.rb
+++ app.rb
@@ -3,15 +3,12 @@ require 'sinatra'
 require 'mongoid'
 
 load 'coder.rb'
-load 'config.ru' 
 
-=begin
 configure do
   Mongoid.configure do |config|
     config.sessions = {:default => {:hosts => ["dbh85.mongolab.com:27857"], :database => "mongotwitter", :username => "c2chang", :password => "tamader9"}}
   end
 end
-=end
 
 get '/' do
   coder = Coder.new(:name => "Chris", :age => 22, :languages => ["Ruby","MongoDB"])
@@ -19,6 +16,6 @@ get '/' do
   ret = Coder.all.to_a
   name = '', age = '', langList = '', langs = ''
   ret.each {|x| full = x, name = x['name'], age = x['age'], langList = x['languages']} 
-  langList.each {|x| langs << "#{x}, "} 
-  "Hi, my name is #{name}, I'm #{age} years young and I hack in #{langs}"
+  langList.each {|x| langs << "#{x} "} 
+  "Hi my name is #{name}, I'm #{age} years young and I hack in #{langs}"
 end
--- config.ru
+++ config.ru
@@ -1,11 +1,5 @@
 # config.ru
 
-configure do
-  Mongoid.configure do |config|
-    config.sessions = {:default => {:hosts => ["dbh85.mongolab.com:27857"], :database => "mongotwitter", :username => "c2chang", :password => "tamader9"}}
-  end
-end
-
 require "./app"
 run Sinatra::Application
 
