6b139309ced74fcb79cce755dc20d5eb0c5cb060
chang.christopher@gmail.com <chang.christopher@gmail.com>
2013-04-30 15:57:05 -0700

    uri test

--- app.rb
+++ app.rb
@@ -4,9 +4,14 @@ require 'mongoid'
 
 load 'coder.rb'
 
+#mongodb://heroku_app15392573:uredidq4ub5ud4c136vjb4jpg7@ds061767.mongolab.com:61767/heroku_app15392573
+
 configure do
   Mongoid.configure do |config|
-    config.sessions = {:default => {:hosts => ["dbh85.mongolab.com:27857"], :database => "mongotwitter", :username => "c2chang", :password => "tamader9"}}
+    config.sessions = {:default => :uri => "mongodb://heroku_app15392573:uredidq4ub5ud4c136vjb4jpg7@ds061767.mongolab.com:61767/heroku_app15392573"}
+=begin
+{:hosts => ["dbh85.mongolab.com:27857"], :database => "mongotwitter", :username => "c2chang", :password => "tamader9"}}
+=end
   end
 end
 
