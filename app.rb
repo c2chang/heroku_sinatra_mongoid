db3cb723e037429ffbe7d6973d64e67776cb4b7c
chang.christopher@gmail.com <chang.christopher@gmail.com>
2013-05-01 14:55:54 -0700

    removed gem mongo

--- app.rb
+++ app.rb
@@ -2,36 +2,21 @@
 require 'sinatra'
 require 'mongoid'
 
+#load your models
 load 'coder.rb'
-=begin
-configure do
-  Mongoid.configure do |config|
-    config.sessions = {:default =>  {:hosts => ["dbh85.mongolab.com:27857"], :database => "mongotwitter", :username => "c2chang", :password => "tamader9"}}
-  end
-end
-=end
 
-puts File.join(File.dirname(__FILE__), "mongoid.yml") 
+#Depending on where your mongoid.yml file resides, you may want to add ,"config" afterthe File.dirname(__FILE__) parameter
 Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"))
 
-=begin
-configure :development do
-  enable :logging, :dump_errors, :run, :sessions
-  #Mongoid.load!(File.join(File.dirname(__FILE__), "config", "mongoid.yml"))
-  puts File.join(File.dirname(__FILE__), "mongoid.yml") 
-  Mongoid.load!(File.join(File.dirname(__FILE__), "mongoid.yml"))
-end
-=end
 
 get '/' do
-  puts 'executing'
   coder = Coder.new(:name => "Chris", :age => 22, :languages => ["Ruby","MongoDB"])
-  puts 'stuck'
   coder.save
-  puts 'not stuck'
+  #Put resulting cursor into an array
   ret = Coder.all.to_a
   name = '', age = '', langList = '', langs = ''
-  ret.each {|x| full = x, name = x['name'], age = x['age'], langList = x['languages']} 
+  ret.each {|x| full = x, name = x['name'], age = x['age'], langList = x['languages']}
+  #<< changes the string in place- cool! 
   langList.each {|x| langs << "#{x} "} 
   "Hi my name is #{name}, I'm #{age} years young and I hack in #{langs}"
 end
